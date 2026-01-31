7370dc6e2dc23496571d32a8564f12d56728abeb
Caique7k <caiquealves7k@gmail.com>
2026-01-31 18:46:27 -0300

    feat: update backend to use Fastify with CORS and static file serving
    - Added @fastify/cors and @fastify/static dependencies.
    - Switched from @nestjs/platform-express to @nestjs/platform-fastify.
    - Configured CORS to allow requests from http://localhost:3000.
    - Registered static file serving for audio files from the 'audios' directory.
    - Loaded environment variables using dotenv.
    
    chore: remove peer dependencies from frontend package-lock.json

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import fastifyStatic from '@fastify/static';
+import fastifyCors from '@fastify/cors';
+import * as path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
-  console.log('Backend rodando em http://localhost:3001');
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  //CORS
+  await app.register(fastifyCors, {
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
+  app
+    .getHttpAdapter()
+    .getInstance()
+    .register(fastifyStatic, {
+      root: path.join(process.cwd(), 'audios'),
+      prefix: '/audios/',
+    });
+  await app.listen(3001, '0.0.0.0');
 }
+
 bootstrap();
